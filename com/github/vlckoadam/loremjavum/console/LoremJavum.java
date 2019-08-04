6a415c49bafa012c49a5cc6ac8a9ba5be0b5e625
Adam Vlčko <48559812+vlckoadam@users.noreply.github.com>
2019-08-04 18:09:47 +0200

    Final version of Lorem Javum Console (part 1)

--- com/github/vlckoadam/loremjavum/console/LoremJavum.java
+++ com/github/vlckoadam/loremjavum/console/LoremJavum.java
@@ -22,13 +22,19 @@ package com.github.vlckoadam.loremjavum.console;
 import com.github.vlckoadam.loremjavum.console.lang.EN;
 import com.github.vlckoadam.loremjavum.console.lang.Lang;
 import com.github.vlckoadam.loremjavum.console.lang.SK;
+import com.github.vlckoadam.loremjavum.core.JavaGeneratorBuilder;
 
+import java.io.File;
 import java.util.Scanner;
+import java.util.UUID;
 
 public class LoremJavum {
     public static Lang lang;
+    //public static boolean hasSession = false;
+
 
     public static void main(String[] args) {
+
         System.out.println("LoremJavum Console Interface Copyright (C) 2019 Adam Vlčko\n" +
                 "    This program comes with ABSOLUTELY NO WARRANTY\n" +
                 "    This is free software, and you are welcome to redistribute it\n" +
@@ -58,5 +64,80 @@ public class LoremJavum {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        boolean cont = true;
+        while (cont){
+            JavaGeneratorBuilder jgb = new JavaGeneratorBuilder();
+            UUID id = UUID.randomUUID();
+            System.out.println(LoremJavum.lang.generatorSessionStarted() + id);
+            System.out.println(LoremJavum.lang.requiredInfo());
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            String rawLevel = "";
+            while (!isNumber(rawLevel) || Integer.parseInt(rawLevel) <= 0){
+                System.out.println("\n[" + id.toString() + "] " + LoremJavum.lang.setLevelOfMassivity()+"\n");
+                rawLevel = scan.nextLine();
+            }
+            int level = Integer.parseInt(rawLevel);
+            System.out.println("\n[" + id.toString() + "] " + LoremJavum.lang.setRootDirectory()+"\n");
+            String dir = scan.nextLine();
+            System.out.println("\n[" + id.toString() + "] " + LoremJavum.lang.setPackage()+"\n");
+            String _package = scan.nextLine();
+            System.out.println("\n[" + id.toString() + "] " + LoremJavum.lang.genParams()+"\n");
+            String gp = scan.nextLine();
+            boolean genParams = (gp.equalsIgnoreCase("a") && LoremJavum.lang instanceof SK)
+                                    || (gp.equalsIgnoreCase("y") && LoremJavum.lang instanceof EN);
+            System.out.println("\n[" + id.toString() + "] " + LoremJavum.lang.genMethodsAndInnerClass()+"\n");
+            String gmaic = scan.nextLine();
+            boolean genMethodsAndInnerClass = (gmaic.equalsIgnoreCase("a") && LoremJavum.lang instanceof SK)
+                    || (gmaic.equalsIgnoreCase("y") && LoremJavum.lang instanceof EN);
+            System.out.println("\n[" + id.toString() + "] " + LoremJavum.lang.jdGuiAntiDecompiler()+"\n");
+            String ad = scan.nextLine();
+            boolean antidecompiler = (ad.equalsIgnoreCase("a") && LoremJavum.lang instanceof SK)
+                    || (ad.equalsIgnoreCase("y") && LoremJavum.lang instanceof EN);
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            for (int i = 0; i < 64; i++) System.out.println();
+            System.out.println("[" + id.toString() + "] " + LoremJavum.lang.generating()+"\n");
+            jgb.level(level).dir(dir)
+                    .generateParams(genParams)
+                    .generateMethodsAndInnerClass(genMethodsAndInnerClass)
+                    .jdGuiAntidecompiler(antidecompiler);
+            if (!_package.contains("-"))
+                jgb.classPackage(_package);
+            jgb.build().generate();
+            System.out.println("[" + id.toString() + "] " + LoremJavum.lang.finished()+"\n");
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(LoremJavum.lang.stats(jgb,id));
+            System.out.println(LoremJavum.lang.wannaContinue());
+            String rCont = scan.nextLine();
+            cont = !rCont.equalsIgnoreCase("n");
+        }
+        System.out.println(LoremJavum.lang.thanks());
+        try {
+            Thread.sleep(5500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < 256; i++) System.out.println();
+        System.out.println(LoremJavum.lang.dg());
+    }
+
+    private static boolean isNumber(String s){
+        try{
+            Integer.parseInt(s);
+        }catch (Exception ex){
+            return false;
+        }
+        return true;
     }
 }
