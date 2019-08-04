74c2319973619954e87271a9a286407e69ee0022
Adam Vlčko <48559812+vlckoadam@users.noreply.github.com>
2019-08-04 18:12:50 +0200

    Final version of Lorem Javum Console (part 2)

--- com/github/vlckoadam/loremjavum/console/lang/EN.java
+++ com/github/vlckoadam/loremjavum/console/lang/EN.java
@@ -19,6 +19,10 @@
  */
 package com.github.vlckoadam.loremjavum.console.lang;
 
+import com.github.vlckoadam.loremjavum.core.JavaGeneratorBuilder;
+
+import java.util.UUID;
+
 public class EN implements Lang {
     @Override
     public String intro() {
@@ -27,5 +31,88 @@ public class EN implements Lang {
                 "                 by Adam Vlcko             \n" +
                 "~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~\n\n\n" +
                 "Starting in 10 seconds... Please wait!";
+    }
+
+    @Override
+    public String generatorSessionStarted() {
+        return "New generation session started with id ";
+    }
+
+    @Override
+    public String requiredInfo() {
+        return "Informations marked with [*] are required.";
+    }
+
+    @Override
+    public String setLevelOfMassivity() {
+        return "[*] Please set the level of massivity (how massive will be the generated code) [between 1 and 2^31]:";
+    }
+
+    @Override
+    public String setRootDirectory() {
+        return "[*] Please set root output directory: ";
+    }
+
+    @Override
+    public String setPackage() {
+        return "    Please set package for generated Java classes: (For skip use \"-\")";
+    }
+
+    @Override
+    public String genParams() {
+        return "    Generate parameters? [y/N]";
+    }
+
+    @Override
+    public String genMethodsAndInnerClass() {
+        return "    Generate methods and inner class? [y/N]";
+    }
+
+    @Override
+    public String jdGuiAntiDecompiler() {
+        return "    Add antidecompiling method for jd-gui version 1.4.0? [y/N]";
+    }
+
+    @Override
+    public String generating() {
+        return "The code is now being generated, please wait! (Higher the level of massivity is and/or more generated things are activated the more time it will take.)  ";
+    }
+
+    @Override
+    public String finished() {
+        return "Generator finished his job! Stats will appear soon: ";
+    }
+
+    @Override
+    public String stats(JavaGeneratorBuilder javaGeneratorBuilder, UUID uuid) {
+        return "°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°\n"
+             + "                                      ~   S T A T S   ~                 \n\n"
+             + "  - UID: " + uuid.toString() + "\n"
+             + "  - LEVEL: " + javaGeneratorBuilder.level()+"\n\n"
+             + "  - Output directory: " + javaGeneratorBuilder.dir()+"\n"
+             + "  - Package: " + (javaGeneratorBuilder.classPackage().equals("") ? "None" : javaGeneratorBuilder.classPackage())+"\n"
+             + "  - Contains parameters: " + (javaGeneratorBuilder.generateParams()? "YES" : "NO") + "\n"
+             + "  - Contains methods and inner class: "  + (javaGeneratorBuilder.generateMethodsAndInnerClass()? "YES" : "NO") + "\n"
+             + "  - Contains JD GUI version 1.4.0 antidecompiling method: " + (javaGeneratorBuilder.jdGuiAntidecompiler()? "YES" : "NO")+ "\n"
+             + "°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°\n\n\n\n";
+
+    }
+
+    @Override
+    public String wannaContinue() {
+        return "That's all. Do you want to continue? :) [Y/n]";
+    }
+
+    @Override
+    public String thanks() {
+        return "Thanks for using this app. See you soon again. Goodbye!";
+    }
+
+    @Override
+    public String dg() {
+        return "//x//x//x//x//x//x//x//x//x//x//x//x//x//x//x//x//x//x//x//x//x//x//x//x//x//x//x//x//x//x//x//\n\n"
+                + "          Copyright.     (c)           A d a m   V l c k o                                   \n"
+                + "                                            2 0 1 9                                          \n\n"
+                + "/x//x//x//x//x//x//x//x//x//x//x//x//x//x//x//x//x//x//x//x//x//x//x//x//x//x//x//x//x//x//x//";
     }
 }
